cff32a4f947b9b4061e589ea10ecfcf5e67d6cda
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-11-08 16:45:51 -0300

    Criando usuarios

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,4 +1,32 @@
+const Login = require('../models/LoginModel');
+
 exports.index = (req, res) => {
     res.render('login');
     return;
+};
+
+exports.register = async (req, res) => {
+    const login = new Login(req.body);
+
+    try {
+        await login.register();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+
+            req.session.save(function () {
+                return res.redirect('/login/index');
+            });
+            return;
+        }
+
+        req.flash('success', 'Seu usuário foi criado com sucesso.');
+        req.session.save(function () {
+            return res.redirect('/login/index');
+        });
+        return;
+    } catch (erro) {
+        console.log(erro);
+        return res.render('404');
+    }
 };
